99bf19ba3f674166099d7036264f112550783429
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2024-12-05 00:57:24 -0600

    Added an option to reboot at the end of the install.   Made the main.sh call the bashrc.sh script

---
+++ install/scripts/reboot.sh
@@ -0,0 +1,11 @@
+echo "A reboot of your system is recommended."
+echo
+if gum confirm "Would you like to reboot now?" ; then
+    gum spin --spinner dot --title "Rebooting now..." -- sleep 3
+    systemctl reboot
+elif [ $? -eq 130 ]; then
+    exit 130
+else
+    echo ":: Reboot skipped"
+fi
+echo ""
