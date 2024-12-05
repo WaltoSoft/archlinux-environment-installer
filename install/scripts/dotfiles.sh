f61a6f6aa73babad2b94657a793e2db491756b75
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2024-12-04 23:58:51 -0600

    Removed line of code that was no longer necessary

--- install/scripts/dotfiles.sh
+++ install/scripts/dotfiles.sh
@@ -2,8 +2,6 @@ executeScript() {
   local dotFilesDirectory=$HOME/.config/hypr
   local installDotFilesDirectory=$INSTALL_DIR/dotfiles/.config/hypr
 
-  mkdir -P $dotFilesDirectory
-
   if [ -d "${dotFilesDirectory}" ]; then
     cp -r $installDotFilesDirectory/* $dotFilesDirectory
   fi
