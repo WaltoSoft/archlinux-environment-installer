fd06951115861b41063492bd780f3e8c639d6a94
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2024-12-05 00:34:43 -0600

    Simplified dotfiles.sh

--- install/scripts/dotfiles.sh
+++ install/scripts/dotfiles.sh
@@ -1,10 +1,5 @@
 executeScript() {
-  local dotFilesDirectory=$HOME/.config/hypr
-  local installDotFilesDirectory=$INSTALL_DIR/dotfiles/.config/hypr
-
-  if [ -d "${dotFilesDirectory}" ]; then
-    cp -r $installDotFilesDirectory/* $dotFilesDirectory
-  fi
+  rsync -avhp -I $INSTALL_DIR/dotfiles/ ~/
 }
 
 executeScript;
