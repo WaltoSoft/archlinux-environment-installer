833b286b2dfec11831784c5a0c887c1eae93c8a9
Billy Walton <165430091+WaltoSoft@users.noreply.github.com>
2024-12-09 13:22:08 -0600

    Fixed variable that didn't get replaced

--- install/scripts/aurPackages.sh
+++ install/scripts/aurPackages.sh
@@ -37,7 +37,6 @@ installPackagesWithYay() {
 installYay() {
   local yayGitFolder="${GIT_DIR}/yay-git"
 
-
   if ! $(isInstalledWithPacman 'yay-git') ; then
     removeExistingFolder $yayGitFolder
     cloneRepo 'https://aur.archlinux.org/yay-git' $yayGitFolder
@@ -52,14 +51,14 @@ installYay() {
       echoText -c $COLOR_RED "ERROR: yay-git could not be compiled"
     fi
 
-    if $(isInstalledWithPacman $packageName) ; then
-      echoText -c $COLOR_GREEN "Package '${packageName}' installed successfully"
+    if $(isInstalledWithPacman 'yay-git') ; then
+      echoText -c $COLOR_GREEN "yay-git installed successfully"
     else
-      echoText -c $COLOR_RED "ERROR: Package '${packageName}' failed to install"
+      echoText -c $COLOR_RED "ERROR: yay-git failed to install"
       exit 1
     fi
   else
-    echoText "yay is already installed!"
+    echoText -c $COLOR_GREEN "yay is already installed!"
   fi
 }
 
